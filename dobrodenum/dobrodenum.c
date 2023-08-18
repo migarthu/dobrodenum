9945391d51fffc29309eb307a643671fc0a012f6
ARTHUR MIGUEL OLIVEIRA ALMEIDA AROS <arthur.aros@a.ucb.br>
2023-08-18 12:18:51 -0300

    dorbrodenum

---
+++ dobrodenum/dobrodenum.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+main(){
+
+int num1, soma;
+
+printf("Digite um numero inteiro para ver seu dobro\n");
+scanf("%d", &num1);
+
+soma = num1 + num1;
+
+printf("\nO dobro de seu numero e: %d", soma);
+
+//ou podeira fazer com multiplicaçao, com o numero digitado vezes o numero 2
+
+//exemplo
+//int num1, mult;
+
+//printf("Digite um numero inteiro para ver seu dobro\n");
+//scanf("%d", &num1);
+
+//mult = num1 * num1;
+
+//printf("\nO dobro de seu numero e: %d", mult);
+
+
+}
